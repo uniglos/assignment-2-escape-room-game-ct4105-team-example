4572bcfaad739516fd98d63a91b59228d38d1bee
JohnGlosUni <jmoss3@glos.ac.uk>
2021-04-30 12:15:42 +0100

    Add car escape puzzle

--- Assets/Scripts/Puzzles/Car Escape/RestartButtonScript.cs
+++ Assets/Scripts/Puzzles/Car Escape/RestartButtonScript.cs
@@ -5,11 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class RestartButtonScript : MonoBehaviour {
 
-	public string nameOfThisScene;
-
 	// Public method to restart scene when Restart button is clicked
 	public void RestartScene()
 	{
-		SceneManager.LoadScene ( nameOfThisScene );
+		SceneManager.LoadScene ("Scene01");
 	}
 }
