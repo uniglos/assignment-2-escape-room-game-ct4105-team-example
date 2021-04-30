1c3e619545e0e44bd91147101c14329249516474
Zayd Dawood <zayd.a.dawood@gmail.com>
2021-04-30 13:18:12 +0100

    Completed Puzzle Game and Linked to Escape Room

---
+++ Assets/Scripts/Puzzles/Car Escape/RestartButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButtonScript : MonoBehaviour {
+
+	public string nameOfThisScene;
+
+	// Public method to restart scene when Restart button is clicked
+	public void RestartScene()
+	{
+		SceneManager.LoadScene ( nameOfThisScene );
+	}
+}
