fa345560fce2125b4c65ee8e850ac857a67a7766
JohnGlosUni <jmoss3@glos.ac.uk>
2021-05-26 16:05:27 +0100

    Revert "Added Combination Lock"
    This reverts commit 3971bf4b73fcdf8fffd22c1f4cb824f0722ffb35.

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -1,24 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class PuzzleManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public List<string> puzzleNames;
 
-    public TextMeshProUGUI combinationLock;
-
-    private string lockCode;
-
     void Start()
     {
 
-        lockCode = combinationLock.text;
-
-        combinationLock.text = "";
-        
         createPuzzleList();
 
     }
@@ -45,8 +36,6 @@ public class PuzzleManager : MonoBehaviour
     public void checkPuzzleStatus()
     {
 
-        int numberOfPuzzlesComplete = 0;
-
         foreach( string puzzle in puzzleNames)
         {
 
@@ -56,16 +45,12 @@ public class PuzzleManager : MonoBehaviour
             {
 
                 status = "Complete";
-
-                numberOfPuzzlesComplete++;
                 
             }
             
             print( "Puzzle " + puzzle + " is: " + status);
 
         }
-    
-        combinationLock.text = lockCode.Substring( 0, numberOfPuzzlesComplete );
 
     }
 
